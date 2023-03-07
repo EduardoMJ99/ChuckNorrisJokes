5bfd56586b173463333e3cb71882fe0d5dce4756
Eduardo Morgado <eduardo.morgado17@tectijuana.edu.mx>
2023-03-06 19:46:05 -0800

    Card design for Chuck Norris jokes created

--- app/src/main/java/com/example/chucknorrisjokes/MainActivity.java
+++ app/src/main/java/com/example/chucknorrisjokes/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.chucknorrisjokes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerJokes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerJokes = findViewById(R.id.recyclerJokes);
     }
 }
